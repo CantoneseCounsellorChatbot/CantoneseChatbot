3c18c857d9a94241576104f0095b5253fc3404c6
CantoneseCounsellorChatbot <79494868+CantoneseCounsellorChatbot@users.noreply.github.com>
2021-06-04 10:31:26 +0800

    Update models.py

--- models.py
+++ models.py
@@ -215,6 +215,7 @@ def general(aa,max_tail_length=10):
   
   
 def chatbot(chatbot_params):
+  print("你好，我係菠蘿仔！我係個專門幫手舒緩考試壓力既輔導員！你有冇咩想同我傾下？")
   chatbot_params["general"]["Threshold"]=1
   chatbot_params["greeting"]["Threshold"]=1
   params_df=pd.DataFrame(chatbot_params).T
