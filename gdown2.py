49f34af11afd59cc5bdd4b7533d37c608a4cc095
kent <liangbaikun@163.com>
2021-06-11 15:06:48 +0800

    update gdown

--- gdown2.py
+++ gdown2.py
@@ -11,9 +11,9 @@ def unzip(filename,extract_path):
     for fileM in zFile.namelist(): 
         zFile.extract(fileM, extract_path)
     zFile.close()
-url_list= ['https://drive.google.com/uc?id=1a24HoNigpqxuJs2oquBtJBbCcdKidZmX',
-            'https://drive.google.com/uc?id=aa',
-            'https://drive.google.com/uc?id=1V3qJIVXFR5HXUXN3q19BwW3gw9wdJKzn',
+url_list= ['https://drive.google.com/uc?id=1YehWJ4BTa_kp5WuZwir5UmnOQGoLvWAn',
+            'https://drive.google.com/uc?id=1ahb97zlnBM57BxyfleaRx-MnsPKCYdOU',
+            'https://drive.google.com/uc?id=1FBCvnKpFoW6XZuE9VMPMEnuMvcAQOLdE',
             'https://drive.google.com/uc?id=1unEqtLPCXzntIev2NZVJ52AouYQp_YpP',
             'https://drive.google.com/uc?id=1u3cJ3u7Lss15fiS4JAp9Zckt1lG_h3b9']
 output_list = ['regression_advice.zip',
@@ -28,7 +28,8 @@ for url,output in zip(url_list,output_list):
     except:
         pass
     os.remove(output)
-# gdown.download(url, output, quiet=False)
+# url= 'https://drive.google.com/uc?id=1YehWJ4BTa_kp5WuZwir5UmnOQGoLvWAn'
+# output = 'pretrained-model.zip'
 # for i in range(100):
 #   print(i)
 #   gdown.download(url, output, quiet=False)
