20f08a1281095185acac6de8a2ded99ecd44e2e0
kent <liangbaikun@163.com>
2021-06-11 16:30:30 +0800

    update gdown

--- gdown2.py
+++ gdown2.py
@@ -4,14 +4,42 @@
 import gdown
 import os
 import zipfile
-import wget
-
+# import wget
+import requests
+import time
 def unzip(filename,extract_path):
     zFile = zipfile.ZipFile(filename, "r")
     #ZipFile.namelist(): 获取ZIP文档内所有文件的名称列表
     for fileM in zFile.namelist(): 
         zFile.extract(fileM, extract_path)
     zFile.close()
+
+
+
+# 进度条模块
+def progressbar(url,path):
+  # if not os.path.exists(path): # 看是否有该文件夹，没有则创建文件夹
+  #   os.mkdir(path)
+  start = time.time() #下载开始时间
+  response = requests.get(url, stream=True)
+  size = 0 #初始化已下载大小
+  chunk_size = 1024 # 每次下载的数据大小
+  content_size = int(response.headers['content-length']) # 下载文件总大小
+  try:
+    if response.status_code == 200: #判断是否响应成功
+      print('Start download,[File size]:{size:.2f} MB'.format(size = content_size / chunk_size /1024)) #开始下载，显示下载文件大小
+      filepath = path 
+      with open(filepath,'wb') as file: #显示进度条
+        for data in response.iter_content(chunk_size = chunk_size):
+          file.write(data)
+          size +=len(data)
+          print('\r'+'[Download progress]:%s%.2f%%' % ('>'*int(size*50/ content_size), float(size / content_size * 100)) ,end=' ')
+    end = time.time() #下载结束时间
+    print('Download completed!,times: %.2f秒' % (end - start)) #输出下载用时时间
+  except:
+    print('Error!')
+
+
 id_list=['1YehWJ4BTa_kp5WuZwir5UmnOQGoLvWAn',
         '1ahb97zlnBM57BxyfleaRx-MnsPKCYdOU',
         '1FBCvnKpFoW6XZuE9VMPMEnuMvcAQOLdE',
@@ -44,13 +72,6 @@ for i in range(len(id_list)):
         unzip(output_list[i],"/content/CantoneseChatbot/")
     except:
         print("download from mega ...")
-        wget.download(mega_list[i], output_list[i])
+        progressbar(mega_list[i], output_list[i])
         unzip(output_list[i],"/content/CantoneseChatbot/")
 print("Dowload finished")
-    # os.remove(output_list[i])
-# url= 'https://drive.google.com/uc?id=1YehWJ4BTa_kp5WuZwir5UmnOQGoLvWAn'
-# output = 'pretrained-model.zip'
-# for i in range(100):
-#   print(i)
-#   gdown.download(url, output, quiet=False)
-#   os.remove('pretrained-model.zip')
